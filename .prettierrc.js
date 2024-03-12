8e839c2a7adcced52399e84914f17b2fe9b4803d
jordanarldt <jordan.arldt@bigcommerce.com>
2024-03-11 19:54:17 -0500

    Add Prettier ESLint

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 90,
+  arrowParens: "avoid",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: false,
+  trailingComma: "all",
+  useTabs: false,
+  tabWidth: 2,
+  parenthesisSpacing: true,
+  plugins: [
+    "prettier-plugin-tailwindcss"
+  ],
+};
